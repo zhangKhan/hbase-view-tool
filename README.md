adf1471bfcb2580a0dd2a73df0a65f61e8eeae99
admin <admin@qq.com>
2019-10-31 15:03:18 +0800

    创建项目基础

---
+++ README.md
@@ -0,0 +1,2 @@
+# hbase-view-tool
+操作hbase数据库的一个可视化平台，这个目的是更好的维护hbase数据库。
---
+++ src/main/java/com/open/connection/DataConnection.java
@@ -0,0 +1,9 @@
+package com.open.connection;
+
+/**
+ * @author zhangli
+ * @Date 2019/10/31 14:48
+ */
+public class DataConnection {
+
+}
