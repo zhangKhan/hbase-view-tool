f3708abdb6396e511a3de048a5152c56c47a1145
admin <admin@qq.com>
2019-10-31 15:02:35 +0800

    添加文件

--- README.md
+++
@@ -1,2 +0,0 @@
-# hbase-view-tool
-操作hbase数据库的一个可视化平台，这个目的是更好的维护hbase数据库。
--- src/main/java/com/open/connection/DataConnection.java
+++
@@ -1,9 +0,0 @@
-package com.open.connection;
-
-/**
- * @author zhangli
- * @Date 2019/10/31 14:48
- */
-public class DataConnection {
-
-}
